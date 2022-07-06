f7453a996a56da933be63513d8db8733004c69e2
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-06 15:15:17 +0900

    initializer

---
+++ src/main/java/com/example/demo/controller/GoodController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/good")
+public class GoodController {
+
+    @GetMapping
+    String isGet() {
+
+        return "get";
+    }
+}
---
+++ src/test/java/com/example/demo/controller/GoodControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(GoodController.class)
+class GoodControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void isGet() throws Exception {
+
+        mvc.perform(get("/good"))
+            .andExpect(status().isOk());
+    }
+}
